65b0e01b8ec75a083c5def5e6ea51d5985ab67ed
vivianzheng <2710680388@qq.com>
2017-07-22 21:19:41 +0800

    增加js

---
+++ Flattening blog/js/main.js
@@ -0,0 +1,37 @@
+$(function(){
+	"use strict";
+	
+	var sidebar = $(".sidebar"),
+		mask = $(".mask"),
+		sidebar_trigger = $("#sidebar_trigger"),
+		backButton = $(".back_to_top");
+	
+	function showSidebar(){
+		mask.fadeIn();
+		sidebar.css("right",0);
+	}
+	
+	function hideSidebar(){
+		mask.fadeOut();
+		sidebar.css("right",-sidebar.width());
+	}
+		
+	sidebar_trigger.on("click",showSidebar);
+	mask.on("click",hideSidebar);
+	
+	backButton.on("click",function(){
+		$("html,body").animate({
+			scrollTop:0
+		},800)
+	});
+	
+	$(window).on("scroll",function(){
+		if($(window).scrollTop() > $(window).height()){
+			backButton.fadeIn();
+		} else {
+			backButton.fadeOut();
+		}
+	});
+	
+	$(window).trigger("scroll");
+})
